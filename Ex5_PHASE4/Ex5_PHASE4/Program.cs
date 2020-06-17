2c46017b5d787c9f2277d49b7031b5c020bac2b9
omunizb <oriolmuniz@gmail.com>
2020-06-17 12:08:08 +0200

    phase 4 ready to submit

---
+++ Ex5_PHASE4/Ex5_PHASE4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ex5_PHASE4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "David", "Sergio", "Maria", "Laura", "Oscar", "Julia", "Oriol" };
+
+            Console.WriteLine("Names that start with 'O':");
+
+            var startingO =
+                from name in names
+                where name.StartsWith("O")
+                select name;
+            
+            foreach (string name in startingO)
+            {
+                Console.WriteLine($"{name}");
+            }
+
+            Console.WriteLine("Names with more than six letters: ");
+
+            var moreThanSix =
+                from name in names
+                where name.Length > 6
+                select name;
+
+            foreach (string name in moreThanSix)
+            {
+                Console.WriteLine($"{name}");
+            }
+
+            Console.WriteLine("Names sorted in descending order: ");
+
+            var descendingOrder =
+                from name in names
+                orderby name descending
+                select name;
+
+            foreach (string name in descendingOrder)
+            {
+                Console.WriteLine($"{name}");
+            }
+        }
+    }
+}
